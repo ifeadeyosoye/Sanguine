a0a3ea836c1a4e512ad46cc27c7d52d9413be1ee
Ife Adeyosoye <adeyosoye.i@northeastern.edu>
2025-12-02 20:42:09 -0500

    userplayer interface

---
+++ src/main/java/sanguine/model/UserPlayer.java
@@ -0,0 +1,11 @@
+package sanguine.model;
+
+/**
+ * UserPlayer is a representation of a player who decides to make a move.
+ *
+ * <p>Represents a human player who clicks on our GUI or inputs commands</p>
+ * or inputs commands or an AI player that automatically decides what actions to take.
+ */
+public interface UserPlayer {
+
+}
