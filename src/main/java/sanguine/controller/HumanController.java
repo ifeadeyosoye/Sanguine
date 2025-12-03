fc5d5223643c752d160a8c9c7c7d161dc173c58e
Kadin Solpon <solpon.k@northeastern.edu>
2025-12-02 20:44:11 -0500

    enum

---
+++ src/main/java/sanguine/controller/HumanController.java
@@ -0,0 +1,4 @@
+package sanguine.controller;
+
+public class HumanController {
+}
---
+++ src/main/java/sanguine/model/TypeOfMove.java
@@ -0,0 +1,9 @@
+package sanguine.model;
+
+/**
+ * REPRESENTS THE TYPE OF MOVE A PLAYER CAN DO. PLAY OR PASS.
+ */
+public enum TypeOfMove {
+  PLAY,
+  PASS
+}
